3afd85a21af4c949f1c2f5c8eac0d0d7c46be69f
Pamod460 <pamodkaushika@gmail.com>
2023-03-03 22:54:52 +0530

    S1V1 initiate
    ->create database
    ->phone shop EER and class diagram and database backup

---
+++ data/Phone_Shop.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: phone_sohop
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `brand`
+--
+
+DROP TABLE IF EXISTS `brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `brand` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brand`
+--
+
+LOCK TABLES `brand` WRITE;
+/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
+INSERT INTO `brand` VALUES (1,'Apple'),(2,'Samsung'),(3,'Oppo'),(4,'Nokia'),(5,'Xiaomi'),(6,'Lg'),(7,'Umidigi'),(8,'Lava'),(9,'Sony');
+/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `image` longtext,
+  `ItemCode` char(6) DEFAULT NULL,
+  ` StatusItem_id` int NOT NULL,
+  `SubCategory_id` int NOT NULL,
+  `brand_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Item_StatuItem_idx` (` StatusItem_id`),
+  KEY `fk_Item_SubCategory1_idx` (`SubCategory_id`),
+  KEY `fk_Item_brand1_idx` (`brand_id`),
+  CONSTRAINT `fk_Item_brand1` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`),
+  CONSTRAINT `fk_Item_StatuItem` FOREIGN KEY (` StatusItem_id`) REFERENCES `statusitem` (`id`),
+  CONSTRAINT `fk_Item_SubCategory1` FOREIGN KEY (`SubCategory_id`) REFERENCES `subcategory` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'M12',54000.00,NULL,'I0001',1,1,2),(2,'A10',34000.00,NULL,'I0002',2,1,2),(3,'A12',54000.00,NULL,'I0003',3,1,2),(4,'Iphone 7',35000.00,NULL,'I0004',1,1,1),(5,'Redmi 9t',45000.00,NULL,'I0005',2,1,5),(6,'Poco M3',45000.00,NULL,'I0006',3,1,5),(7,'Poco X3',80000.00,NULL,'I0007',1,1,5),(8,'Oppo F7',40000.00,NULL,'I0008',2,1,3),(9,'i12',3000.00,NULL,'I0009',3,2,1);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `statusitem`
+--
+
+DROP TABLE IF EXISTS `statusitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `statusitem` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `statusitem`
+--
+
+LOCK TABLES `statusitem` WRITE;
+/*!40000 ALTER TABLE `statusitem` DISABLE KEYS */;
+INSERT INTO `statusitem` VALUES (1,'Available'),(2,'Out of Stock'),(3,'Limited Stock');
+/*!40000 ALTER TABLE `statusitem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subcategory`
+--
+
+DROP TABLE IF EXISTS `subcategory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subcategory` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subcategory`
+--
+
+LOCK TABLES `subcategory` WRITE;
+/*!40000 ALTER TABLE `subcategory` DISABLE KEYS */;
+INSERT INTO `subcategory` VALUES (1,'Phone'),(2,'HandFree'),(3,'Back Cover'),(4,'Battery');
+/*!40000 ALTER TABLE `subcategory` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-03 22:47:08
