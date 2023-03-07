d5f5671b7655b1ecc5ea2147664cd23b3f07ef8e
Pamod460 <pamodkaushika@gmail.com>
2023-03-07 22:01:25 +0530

    Update Phone_Shop.sql

--- data/Phone_Shop.sql
+++ data/Phone_Shop.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE  IF NOT EXISTS `phone_sohop` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `phone_sohop`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
